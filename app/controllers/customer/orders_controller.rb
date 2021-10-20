09b9892b266e6e11a9a493a4f8f6b9909e48acc6
akiho19009 <mte.bn.1537.g@gmail.com>
2021-10-20 04:45:00 +0000

    オーダー途中です

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,2 +1,15 @@
 class Customer::OrdersController < ApplicationController
+
+  validates :payment, presence: true
+  validates :address, presence: true
+  
+  def new
+    @order = Order.new
+  # モデルクラスのインスタンス→保存したいテーブルのインスタンスのこと、
+  # 今回はordersテーブルに新たにレコードを作成するためこの記述
+  # コントローラで作成したインスタンスがnewメソッドで新たに作成されて何も情報を持っていなければ自動的にcreateアクションへ、
+  # findメソッドなどで作成され、すでに情報を持っている場合はupdateアクションへ自動的に振り分けてくれる
+  
+  end
+
 end
