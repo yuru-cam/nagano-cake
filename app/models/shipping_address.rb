4e9a52bf1a0a3261d7d169484d0c61059e184401
kazarixx <tua.ih8@gmail.com>
2021-10-21 12:21:21 +0000

    [update]

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,12 @@
 class ShippingAddress < ApplicationRecord
+  belongs_to :customer
+  
+  validates :shipping_postcode, length: {is: 7}, presence: true
+  validates :shipping_address, presence: true
+  validates :shipping_name, presence: true
+  
+  def shipping_address_for_view
+  	self.postal_code + "　" + self.address + "　" + self.name
+  end
+
 end
