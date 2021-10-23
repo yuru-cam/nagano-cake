bf8baee358c3d2ca053a549829e5f2876019a072
kameinaoki <nkamegre4nhns9@gmail.com>
2021-10-23 03:53:28 +0000

    [完了(レイアウト以外)会員情報編集画面]

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,4 +1,21 @@
 class Customer::CustomersController < ApplicationController
+    
+  def edit
+    @customer = current_customer
+  end
+    
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_path
+  end
+  
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
+  end
   
   
 end
