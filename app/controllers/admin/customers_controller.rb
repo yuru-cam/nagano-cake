78114e8c7c1ba4028887048e3edaf9cb9606d7e5
kameinaoki <nkamegre4nhns9@gmail.com>
2021-10-24 08:06:31 +0000

    [完了(レイアウト以外)管理者側会員情報編集ページ]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,8 +13,8 @@ class Admin::CustomersController < ApplicationController
   end
   
   def update
-    p @customer = Customer.find(params[:id])
-    p @customer.update(customer_params)
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
     redirect_to admin_customer_path(@customer.id)
   end
   
