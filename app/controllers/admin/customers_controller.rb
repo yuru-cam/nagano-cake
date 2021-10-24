0a0f3e0ba6f54503e2ad9d63edb2799fe14d40f1
kameinaoki <nkamegre4nhns9@gmail.com>
2021-10-24 05:36:59 +0000

    [ラジオボタンがデータ保存されないだけ]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,4 +8,19 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
     
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def updated
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+  
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:full_name, :full_name_kana, :postcode, :address, :phone_number, :email, :is_deleted)
+  end
 end
