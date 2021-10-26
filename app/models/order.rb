37019301d75e99b1f2245174b2edf45830dad39a
kazarixx <tua.ih8@gmail.com>
2021-10-26 05:53:18 +0000

    [update]モデルの記入等

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
 # enamuを利用した支払方法
   enum payment_method: { credit_card: 0, transfer: 1 }
 # 注文ステータスの記述
-  enum status:  {"入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
+  enum order_status:  {"入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
 
   # enum address_method: {"ご自身の住所": 0, "登録済みの住所から選択": 1, "新しいお届け先": 2}
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,10 @@
 class OrderDetail < ApplicationRecord
     belongs_to :item
     belongs_to :order
+    
+    enum making_status: {
+        "製作不可":0,
+        "製作待ち":1,
+        "製作中":2,
+        "製作終了":3 }
 end
