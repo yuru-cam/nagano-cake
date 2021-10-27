24270432f89da2131d7da02515baa150b1a53450
kazarixx <tua.ih8@gmail.com>
2021-10-27 09:33:58 +0000

    [update]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,6 +13,9 @@ class Admin::OrdersController < ApplicationController
   def show
   	@order = Order.find(params[:id])
     @order_details = @order.order_details
+    @sum = 0
+    
+    
   end
 
   def update
